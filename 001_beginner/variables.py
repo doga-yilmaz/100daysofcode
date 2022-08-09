4d16d9d66501e03230e6f82ec165b13e4c16aa43
furth3r <110274753+doga-yilmaz@users.noreply.github.com>
2022-08-09 20:38:44 +0300

    Create variables.py

---
+++ 001_beginner/variables.py
@@ -0,0 +1,20 @@
+# i wanna change the variable's values
+from re import X
+
+
+x = 12
+y = 100
+
+#if x == 12:
+    #x = 100
+    #y = 12
+#print (x,y)
+
+#okey okey i'm kidding.
+
+c = x
+x = y
+y = c
+
+print (x,y)
+
